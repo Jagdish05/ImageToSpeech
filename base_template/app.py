dd43ded38e414c8e16995c3f8f14c7a0e08d36ea
Jagdish Katariya <katariyajagdish05@gmail.com>
2021-02-02 15:58:33 -0600

    Delete base_template directory

--- base_template/app.py
+++
@@ -1,8 +0,0 @@
-#!flask/bin/python
-from flask import Flask, render_template
-app = Flask(__name__)
-@app.route('/index', methods=('GET', 'POST'))
-def index():
-    return render_template('index.html')
-if __name__ == '__main__':
-    app.run(host="localhost",port="8000",debug=True)
